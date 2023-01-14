a98c9dfd931a203f55a1e7eefcab210b72d1d4f7
bakermor <bakermor@oregonstate.edu>
2023-01-14 00:01:59 -0600

    database setup

--- mcc_predictions.py
+++ mcc_predictions.py
@@ -1,16 +1,18 @@
-<<<<<<< HEAD
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="P4ssw0rd",
-    database="mccdb"
-)
 
-mycursor = mydb.cursor()
+def main():
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="P4ssw0rd",
+        database="mccdb"
+    )
 
-mydb.commit()
-=======
+    mycursor = mydb.cursor()
 
->>>>>>> origin/main
+    mydb.commit()
+
+
+main()
+
