7e881cc95acb39f7e59aa198e7a691979780314c
bakermor <bakermor@oregonstate.edu>
2023-02-17 12:58:06 -0600

    Moved routes for ComparePlayers.html and DisplayCompareP.html to blueprint ui_multiple_players.py

--- ui_main.py
+++ ui_main.py
@@ -1,12 +1,13 @@
 from flask import Flask, redirect, url_for, render_template, request
 import requests
 import json
-from compare_players import *
 from run_event import *
 from ui_single_player import single_player
+from ui_multiple_players import multiple_players
 
 app = Flask(__name__)
 app.register_blueprint(single_player, url_prefix="")
+app.register_blueprint(multiple_players, url_prefix="")
 
 TEAMS = ['Red Rabbits', 'Orange Ocelots', 'Yellow Yaks', 'Lime Llamas', 'Green Geckos', 'Cyan Coyotes', 'Aqua Axolotls',
          'Blue Bats', 'Purple Pandas', 'Pink Parrots']
@@ -20,59 +21,6 @@ def home():
 @app.route("/valid")
 def valid_players():
     return render_template("ValidPlayers.html", teamIcons=TEAMS)
-
-# Compare Players
-@app.route("/compare-players", methods=["POST","GET"])
-def compare_p():
-    # Process form data
-    if request.method == "POST":
-        req_players = request.form.getlist('pname')
-        for i in range(len(req_players)):
-            if req_players[i] == "RANDOM":
-                response = requests.get("http://127.0.0.1:5001/player")
-                req_players[i] = response.json()[0]
-
-        content = compare_players(req_players)
-        if content[0] == 'invalid':
-            return render_template("DisplayCompareP.html", p_names=req_players, error=content[1], teamIcons=TEAMS,
-                                   num_p = len(req_players))
-
-        return redirect(url_for("display_players", content=json.dumps(content)))
-
-    # Load initial webpage
-    else:
-        return render_template("ComparePlayers.html", count=2, teamIcons=TEAMS)
-
-# Display Compare Players
-@app.route("/display-p/<content>", methods=["POST", "GET"])
-def display_players(content):
-    if request.method == "POST":
-        pass
-
-    else:
-        content = json.loads(content)
-
-        p_names = []
-        img_paths = []
-        game_lst = set()
-        data = {}
-
-        for p in content:
-            p_names.append(p["Player"][0])
-            img_paths.append("/images/" + str(p["Player"][1]) + ".png")
-            game_lst.update(p.keys())
-
-            p_game = {}
-            for key in p:
-                if key != "Player":
-                    p_game[key] = (int(p[key][0]), int(p[key][1]))
-            data[p["Player"][0]] = p_game
-        game_lst.remove("Player")
-        game_lst.remove("Overall")
-
-        return render_template("DisplayCompareP.html", p_names=p_names, imgs=img_paths, games=game_lst,
-                               num_p=len(p_names), data=data, coins=0, teamIcons=TEAMS)
-
 
 # Compare Teams
 @app.route("/compare-teams", methods=["POST","GET"])
---
+++ ui_multiple_players.py
@@ -0,0 +1,63 @@
+from flask import Blueprint, render_template, redirect, url_for, request
+import json
+import requests
+from compare_players import *
+
+multiple_players = Blueprint("multiple_players", __name__, static_folder="static", template_folder="templates")
+
+TEAMS = ['Red Rabbits', 'Orange Ocelots', 'Yellow Yaks', 'Lime Llamas', 'Green Geckos', 'Cyan Coyotes', 'Aqua Axolotls',
+         'Blue Bats', 'Purple Pandas', 'Pink Parrots']
+
+
+@multiple_players.route("/compare-players", methods=["POST","GET"])
+def compare_p():
+    if request.method == "GET":
+        return render_template("ComparePlayers.html", count=2, teamIcons=TEAMS)
+
+    # Process Form Data
+    if request.method == "POST":
+        req_players = request.form.getlist('pname')
+        # Handles Random Requests
+        for i in range(len(req_players)):
+            if req_players[i] == "RANDOM":
+                response = requests.get("http://127.0.0.1:5001/player")
+                req_players[i] = response.json()[0]
+
+        content = compare_players(req_players)
+        # Handles Invalid Requests
+        if content[0] == 'invalid':
+            return render_template("DisplayCompareP.html", p_names=req_players, error=content[1], teamIcons=TEAMS,
+                                   num_p = len(req_players))
+
+        return redirect(url_for("multiple_players.display_players", content=json.dumps(content)))
+
+
+# Display Compare Players
+@multiple_players.route("/display-p/<content>", methods=["POST", "GET"])
+def display_players(content):
+    if request.method == "POST":
+        pass
+
+    else:
+        p_names, img_paths = [], []
+        game_lst = set()
+        data = {}
+
+        content = json.loads(content)
+        for p in content:
+            p_names.append(p["Player"][0])
+            img_paths.append("/images/" + str(p["Player"][1]) + ".png")
+            game_lst.update(p.keys())
+
+            # data[Player Name] = {Game : Coins,Placement, ...} for each game played
+            p_game = {}
+            for key in p:
+                if key != "Player":
+                    p_game[key] = (int(p[key][0]), int(p[key][1]))
+            data[p["Player"][0]] = p_game
+
+        game_lst.remove("Player")
+        game_lst.remove("Overall")
+
+        return render_template("DisplayCompareP.html", p_names=p_names, imgs=img_paths, games=game_lst,
+                               num_p=len(p_names), data=data, coins=0, teamIcons=TEAMS)
